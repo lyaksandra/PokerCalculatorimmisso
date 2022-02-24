1828fe47c9714d360d2b7e8226f280b00a74d5bd
Alexandra <5279424@gmail.com>
2022-02-24 17:47:10 +0200

    commit10

---
+++ app/src/main/java/com/example/pokercalculatorimmisso/GridLayoutManager.java
@@ -0,0 +1,14 @@
+package com.example.pokercalculatorimmisso;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GridLayoutManager extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grid_layout_manager);
+    }
+}
